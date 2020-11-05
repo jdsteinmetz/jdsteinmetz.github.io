d19c7f3c4e58b326ccb288bb2936069c3bb2210c
jdsteinmetz <71454224+jdsteinmetz@users.noreply.github.com>
2020-11-05 00:42:20 -0500

    Big Update for Project 2

---
+++ project-2/assets/toggle.js
@@ -0,0 +1,29 @@
+$(document).ready(function (){
+    $('#vine-button').click(function() {
+        $("#vine").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    $('#bridge-button').click(function() {
+        $("#bridge").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    $('#millrace-button').click(function() {
+        $("#millrace").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    $('#orchard-button').click(function() {
+        $("#orchard").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    $('#fidler-button').click(function() {
+        $("#fidler").toggleClass("visible");
+        });
+});
