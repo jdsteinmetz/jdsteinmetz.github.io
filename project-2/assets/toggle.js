df7455dff51159348a7b3baba3780815106b003e
jdsteinmetz <71454224+jdsteinmetz@users.noreply.github.com>
2020-11-05 11:24:49 -0500

    Updating Toggle Function

--- project-2/assets/toggle.js
+++ project-2/assets/toggle.js
@@ -1,29 +1,49 @@
 $(document).ready(function (){
     $('#vine-button').click(function() {
         $("#vine").toggleClass("visible");
+        $("#bridge").removeClass("visible");
+        $("#millrace").removeClass("visible");
+        $("#orchard").removeClass("visible");
+        $("#fidler").removeClass("visible");
         });
 });
 
 $(document).ready(function (){
     $('#bridge-button').click(function() {
         $("#bridge").toggleClass("visible");
+        $("#vine").removeClass("visible");
+        $("#millrace").removeClass("visible");
+        $("#orchard").removeClass("visible");
+        $("#fidler").removeClass("visible");
         });
 });
 
 $(document).ready(function (){
     $('#millrace-button').click(function() {
         $("#millrace").toggleClass("visible");
+        $("#bridge").removeClass("visible");
+        $("#vine").removeClass("visible");
+        $("#orchard").removeClass("visible");
+        $("#fidler").removeClass("visible");
         });
 });
 
 $(document).ready(function (){
     $('#orchard-button').click(function() {
         $("#orchard").toggleClass("visible");
+        $("#bridge").removeClass("visible");
+        $("#millrace").removeClass("visible");
+        $("#vine").removeClass("visible");
+        $("#fidler").removeClass("visible");
         });
 });
 
 $(document).ready(function (){
     $('#fidler-button').click(function() {
         $("#fidler").toggleClass("visible");
+        $("#bridge").removeClass("visible");
+        $("#millrace").removeClass("visible");
+        $("#orchard").removeClass("visible");
+        $("#vine").removeClass("visible");
         });
 });
